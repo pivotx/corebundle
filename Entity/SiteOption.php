befa934d90172e5ffb5baa207e3d29ea8fb7ae0a
Marcel Wouters <marcel@twokings.nl>
2012-08-31 16:38:02 +0200

    Added mediatype to SiteOption.

--- Entity/SiteOption.php
+++ Entity/SiteOption.php
@@ -42,7 +42,210 @@ class SiteOption
     private $autoload;
 
     /**
+     * @var string $mediatype
+     */
+    private $mediatype;
+
+    /**
      * @var text $value
      */
     private $value;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set sitename
+     *
+     * @param string $sitename
+     */
+    public function setSitename($sitename)
+    {
+        $this->sitename = $sitename;
+    }
+
+    /**
+     * Get sitename
+     *
+     * @return string 
+     */
+    public function getSitename()
+    {
+        return $this->sitename;
+    }
+
+    /**
+     * Set groupname
+     *
+     * @param string $groupname
+     */
+    public function setGroupname($groupname)
+    {
+        $this->groupname = $groupname;
+    }
+
+    /**
+     * Get groupname
+     *
+     * @return string 
+     */
+    public function getGroupname()
+    {
+        return $this->groupname;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set date_created
+     *
+     * @param datetime $dateCreated
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->date_created = $dateCreated;
+    }
+
+    /**
+     * Get date_created
+     *
+     * @return datetime 
+     */
+    public function getDateCreated()
+    {
+        return $this->date_created;
+    }
+
+    /**
+     * Set date_modified
+     *
+     * @param datetime $dateModified
+     */
+    public function setDateModified($dateModified)
+    {
+        $this->date_modified = $dateModified;
+    }
+
+    /**
+     * Get date_modified
+     *
+     * @return datetime 
+     */
+    public function getDateModified()
+    {
+        return $this->date_modified;
+    }
+
+    /**
+     * Set autoload
+     *
+     * @param boolean $autoload
+     */
+    public function setAutoload($autoload)
+    {
+        $this->autoload = $autoload;
+    }
+
+    /**
+     * Get autoload
+     *
+     * @return string 
+     */
+    public function getAutoload()
+    {
+        return $this->autoload;
+    }
+
+    /**
+     * Set mediatype
+     *
+     * @param string $mediatype
+     */
+    public function setMediatype($mediatype)
+    {
+        $this->mediatype = $mediatype;
+    }
+
+    /**
+     * Get mediatype
+     *
+     * @return string 
+     */
+    public function getMediatype()
+    {
+        return $this->mediatype;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+
+    /**
+     * Crud defaults
+     */
+
+    /**
+     * Get mediatype type
+     */
+    public function getCrudFormType_mediatype()
+    {
+        return 'choice';
+    }
+
+    /**
+     * Get mediatype choices
+     */
+    public function getCrudFormChoices_mediatype()
+    {
+        return array(
+            'text/x-line' => 'Single line',
+            'text/plain' => 'Multiple lines',
+            'text/html' => 'HTML',
+            'text/xml' => 'XML',
+            'text/x-yaml' => 'YAML',
+            'application/json' => 'JSON',
+        );
+    }
 }
