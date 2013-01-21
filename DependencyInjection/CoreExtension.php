0ebe467bb49d5d0dce43e38de891e3ca6dff424a
Marcel Wouters <marcel@twokings.nl>
2013-01-21 11:38:41 +0100

    Moved src/PivotX to PivotX/

--- DependencyInjection/CoreExtension.php
+++ DependencyInjection/CoreExtension.php
@@ -20,11 +20,8 @@ class CoreExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        //echo "Reading core services.xml\n";
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        //$loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(dirname(dirname(dirname(dirname(__DIR__)))).'/Resources/config'));
         $loader->load('services.xml');
 
         //echo 'Loading services in PivotX\CoreBundle\DependencyInjection\PivotXCoreExtension..'."\n";
